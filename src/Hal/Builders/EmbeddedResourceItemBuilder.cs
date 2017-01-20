c021fd51a29ff7490aad20355923d161a9ab919f
daxnet <daxnet@outlook.com>
2017-01-20 16:13:30 +0800

    Added the comments.

--- src/Hal/Builders/EmbeddedResourceItemBuilder.cs
+++ src/Hal/Builders/EmbeddedResourceItemBuilder.cs
@@ -1,28 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ---------------------------------------------------------------------------
+//  _    _          _      
+// | |  | |   /\   | |     
+// | |__| |  /  \  | |     
+// |  __  | / /\ \ | |     
+// | |  | |/ ____ \| |____ 
+// |_|  |_/_/    \_\______|
+//
+// A C#/.NET Core implementation of Hypertext Application Language
+// http://stateless.co/hal_specification.html
+// 
+// MIT License
+//
+// Copyright (c) 2017 Sunny Chen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ---------------------------------------------------------------------------
+
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hal.Builders
 {
+    /// <summary>
+    /// Represents that the implemented classes are HAL resource builders that
+    /// will add an embedded resource to the embedded resource collection of
+    /// the building HAL resource.
+    /// </summary>
+    /// <seealso cref="Hal.Builders.IBuilder" />
     public interface IEmbeddedResourceItemBuilder : IBuilder
     {
+        /// <summary>
+        /// Gets the name of the embedded resource collection.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         string Name { get; }
     }
 
+    /// <summary>
+    /// Represents an internal implementation of the <see cref="IEmbeddedResourceItemBuilder"/> class.
+    /// </summary>
+    /// <seealso cref="Hal.Builders.Builder" />
+    /// <seealso cref="Hal.Builders.IEmbeddedResourceItemBuilder" />
     internal sealed class EmbeddedResourceItemBuilder : Builder, IEmbeddedResourceItemBuilder
     {
+        #region Private Fields
         private readonly string name;
         private readonly IBuilder resourceBuilder;
+        #endregion
 
+        #region Ctor        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmbeddedResourceItemBuilder"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="name">The name of the embedded resource collection.</param>
+        /// <param name="resourceBuilder">The resource builder that will build the embedded resource.</param>
         public EmbeddedResourceItemBuilder(IBuilder context, string name, IBuilder resourceBuilder) : base(context)
         {
             this.name = name;
             this.resourceBuilder = resourceBuilder;
         }
+        #endregion
 
+        #region Public Properties        
+        /// <summary>
+        /// Gets the name of the embedded resource collection.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name => this.name;
+        #endregion
 
+        #region Protected Methods        
+        /// <summary>
+        /// Builds the <see cref="Resource" /> instance.
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <returns>
+        /// The <see cref="Resource" /> instance to be built.
+        /// </returns>
         protected override Resource DoBuild(Resource resource)
         {
             var embeddedResource = resource.EmbeddedResources.First(x => x.Name.Equals(this.name));
@@ -45,5 +119,6 @@ namespace Hal.Builders
 
             return resource;
         }
+        #endregion
     }
 }
--- src/Hal/Builders/Extensions.cs
+++ src/Hal/Builders/Extensions.cs
@@ -1,42 +1,122 @@
-﻿using System;
+﻿// ---------------------------------------------------------------------------
+//  _    _          _      
+// | |  | |   /\   | |     
+// | |__| |  /  \  | |     
+// |  __  | / /\ \ | |     
+// | |  | |/ ____ \| |____ 
+// |_|  |_/_/    \_\______|
+//
+// A C#/.NET Core implementation of Hypertext Application Language
+// http://stateless.co/hal_specification.html
+// 
+// MIT License
+//
+// Copyright (c) 2017 Sunny Chen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ---------------------------------------------------------------------------
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hal.Builders
 {
+    /// <summary>
+    /// Represents the class that provides the extension methods to form
+    /// the Fluent API of the building of HAL resources.
+    /// </summary>
     public static class Extensions
     {
-        #region IResourceBuilder Extensions
+        #region IResourceBuilder Extensions        
+        /// <summary>
+        /// Assigns the object state to the resource that is going to be built.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="state">The object state.</param>
+        /// <returns></returns>
         public static IResourceStateBuilder WithState(this IResourceBuilder builder, object state)
         {
             return new ResourceStateBuilder(builder, state);
         }
         #endregion
 
-        #region IResourceStateBuilder Extensions
+        #region IResourceStateBuilder Extensions        
+        /// <summary>
+        /// Adds a link to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="rel">The relation of the resource location.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddLink(this IResourceStateBuilder builder, string rel)
         {
             return new LinkBuilder(builder, rel, false);
         }
 
+        /// <summary>
+        /// Adds the "self" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddSelfLink(this IResourceStateBuilder builder)
         {
             return new LinkBuilder(builder, "self", false);
         }
 
+        /// <summary>
+        /// Adds the "curies" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddCuriesLink(this IResourceStateBuilder builder)
         {
             return new LinkBuilder(builder, "curies", true);
         }
 
+        /// <summary>
+        /// Adds the embedded resource collection to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="name">The name of the embedded resource collection.</param>
+        /// <returns></returns>
         public static IEmbeddedResourceBuilder AddEmbedded(this IResourceStateBuilder builder, string name)
         {
             return new EmbeddedResourceBuilder(builder, name);
         }
         #endregion
 
-        #region ILinkBuilder Extensions
+        #region ILinkBuilder Extensions        
+        /// <summary>
+        /// Adds a link item to the currently building link.
+        /// </summary>
+        /// <param name="linkBuilder">The link builder.</param>
+        /// <param name="href">The href attribute of a link item.</param>
+        /// <param name="name">The name attribute of a link item.</param>
+        /// <param name="templated">The <see cref="bool"/> value which indicates if the <c>Href</c> property
+        /// is a URI template.</param>
+        /// <param name="type">The media type expected when dereferencing the target source.</param>
+        /// <param name="deprecation">The URL which provides further information about the deprecation.</param>
+        /// <param name="profile">The URI that hints about the profile of the target resource.</param>
+        /// <param name="title">The <see cref="string"/> value which is intended for labelling
+        /// the link with a human-readable identifier.</param>
+        /// <param name="hreflang">The <see cref="string"/> value which is intending for indicating
+        /// the language of the target resource.</param>
+        /// <param name="additionalProperties">The additional properties.</param>
+        /// <returns></returns>
         public static ILinkItemBuilder WithLinkItem(this ILinkBuilder linkBuilder, string href,
             string name = null, bool? templated = null, string type = null,
             string deprecation = null, string profile = null, string title = null,
@@ -49,21 +129,55 @@ namespace Hal.Builders
         #endregion
 
         #region ILinkItemBuilder Extensions
+
+        /// <summary>
+        /// Adds a link to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="rel">The relation of the resource location.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddLink(this ILinkItemBuilder builder, string rel)
         {
             return new LinkBuilder(builder, rel, false);
         }
 
+        /// <summary>
+        /// Adds the "self" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddSelfLink(this ILinkItemBuilder builder)
         {
             return new LinkBuilder(builder, "self", false);
         }
 
+        /// <summary>
+        /// Adds the "curies" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddCuriesLink(this ILinkItemBuilder builder)
         {
             return new LinkBuilder(builder, "curies", true);
         }
 
+        /// <summary>
+        /// Adds a link item to the currently building link.
+        /// </summary>
+        /// <param name="linkBuilder">The link builder.</param>
+        /// <param name="href">The href attribute of a link item.</param>
+        /// <param name="name">The name attribute of a link item.</param>
+        /// <param name="templated">The <see cref="bool"/> value which indicates if the <c>Href</c> property
+        /// is a URI template.</param>
+        /// <param name="type">The media type expected when dereferencing the target source.</param>
+        /// <param name="deprecation">The URL which provides further information about the deprecation.</param>
+        /// <param name="profile">The URI that hints about the profile of the target resource.</param>
+        /// <param name="title">The <see cref="string"/> value which is intended for labelling
+        /// the link with a human-readable identifier.</param>
+        /// <param name="hreflang">The <see cref="string"/> value which is intending for indicating
+        /// the language of the target resource.</param>
+        /// <param name="additionalProperties">The additional properties.</param>
+        /// <returns></returns>
         public static ILinkItemBuilder WithLinkItem(this ILinkItemBuilder builder, string href,
             string name = null, bool? templated = null, string type = null,
             string deprecation = null, string profile = null, string title = null,
@@ -74,13 +188,25 @@ namespace Hal.Builders
                 type, deprecation, profile, title, hreflang, builder.EnforcingArrayConverting, additionalProperties);
         }
 
+        /// <summary>
+        /// Adds the embedded resource collection to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="name">The name of the embedded resource collection.</param>
+        /// <returns></returns>
         public static IEmbeddedResourceBuilder AddEmbedded(this ILinkItemBuilder builder, string name)
         {
             return new EmbeddedResourceBuilder(builder, name);
         }
         #endregion
 
-        #region IEmbeddedResourceBuilder Extensions
+        #region IEmbeddedResourceBuilder Extensions        
+        /// <summary>
+        /// Adds the embedded resource to the embedded resource collection of the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="resourceBuilder">The resource builder that will build the embedded resource.</param>
+        /// <returns></returns>
         public static IEmbeddedResourceItemBuilder Resource(this IEmbeddedResourceBuilder builder, IBuilder resourceBuilder)
         {
             return new EmbeddedResourceItemBuilder(builder, builder.Name, resourceBuilder);
@@ -88,26 +214,55 @@ namespace Hal.Builders
         #endregion
 
         #region IEmbeddedResourceItemBuilder Extensions
+
+        /// <summary>
+        /// Adds the embedded resource to the embedded resource collection of the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="resourceBuilder">The resource builder that will build the embedded resource.</param>
+        /// <returns></returns>
         public static IEmbeddedResourceItemBuilder Resource(this IEmbeddedResourceItemBuilder builder, IBuilder resourceBuilder)
         {
             return new EmbeddedResourceItemBuilder(builder, builder.Name, resourceBuilder);
         }
 
+        /// <summary>
+        /// Adds a link to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="rel">The relation of the resource location.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddLink(this IEmbeddedResourceItemBuilder builder, string rel)
         {
             return new LinkBuilder(builder, rel, false);
         }
 
+        /// <summary>
+        /// Adds the "self" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddSelfLink(this IEmbeddedResourceItemBuilder builder)
         {
             return new LinkBuilder(builder, "self", false);
         }
 
+        /// <summary>
+        /// Adds the "curies" link.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
         public static ILinkBuilder AddCuriesLink(this IEmbeddedResourceItemBuilder builder)
         {
             return new LinkBuilder(builder, "curies", true);
         }
 
+        /// <summary>
+        /// Adds the embedded resource collection to the building resource.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="name">The name of the embedded resource collection.</param>
+        /// <returns></returns>
         public static IEmbeddedResourceBuilder AddEmbedded(this IEmbeddedResourceItemBuilder builder, string name)
         {
             return new EmbeddedResourceBuilder(builder, name);
