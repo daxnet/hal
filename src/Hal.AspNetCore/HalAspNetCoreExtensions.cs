730045d5ac35307435bdfb7229a59e916517d72b
daxnet <sunny.chen@perkinelmer.com>
2023-01-06 19:55:52 +0800

    Fixed the unit test issue that requires .net 7.

--- src/Hal.AspNetCore/HalAspNetCoreExtensions.cs
+++ src/Hal.AspNetCore/HalAspNetCoreExtensions.cs
@@ -90,28 +90,6 @@ namespace Hal.AspNetCore
             return serviceCollection;
         }
 
-        internal static string Dump(this HttpRequest httpRequest)
-        {
-            var sb = new StringBuilder();
-            var properties = from p in typeof(HttpRequest)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                             where p.CanRead
-                             select p;
-            foreach(var p in properties)
-            {
-                try
-                {
-                    sb.AppendLine($"{p.Name} = {p.GetValue(httpRequest)}");
-                }
-                catch { continue; }
-            }
-
-            sb.AppendLine("Headers:");
-            httpRequest.Headers.ToList()
-                .ForEach(h => sb.AppendLine($"  {h.Key} = {h.Value}"));
-
-            return sb.ToString();
-        }
         #endregion Public Methods
 
     }
